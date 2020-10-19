754cc3aab3a3c3e9473ab310647dea827a9fc2bc
opilane <opilane@HP-CZC6297B8P>
2020-10-19 13:31:09 +0300

    123

--- Stranici/Stranici/Mai.xaml.cs
+++ Stranici/Stranici/Mai.xaml.cs
@@ -15,6 +15,28 @@ namespace Stranici
         public Mai()
         {
             InitializeComponent();
+            Grid grd = new Grid
+            {
+                RowDefinitions =
+                {
+                       new RowDefinition {Height=new GridLength(1,GridUnitType.Star)},
+                       new RowDefinition { Height = new GridLength(3, GridUnitType.Star) },
+                       new RowDefinition { Height = new GridLength(2, GridUnitType.Star) }
+
+
+                },
+                ColumnDefinitions =
+                {
+                      new ColumnDefinition{Width=new GridLength(1,GridUnitType.Star)}
+                }
+            };
+            Label nimetus = new Label { Text = "Märtsi Pilt", FontSize = 30 };
+            Image img = new Image { Source = "marts.jpg" };
+            Label kirjeldus = new Label { Text = "Mai on viies kuu!!! \nKolmas kevade kuu!!!" };
+            grd.Children.Add(nimetus, 0, 0);
+            grd.Children.Add(img, 0, 1);
+            grd.Children.Add(kirjeldus, 0, 2);
+            Content = grd;
         }
     }
 }
